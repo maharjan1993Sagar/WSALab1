3ad4063cd0d15e125f47992a14e49dd24589e1c8
Your Name <maharjanakancha@gmail.com>
2019-11-26 07:13:02 +0545

    Second

--- WSALab1/Lab1Q2.cs
+++ WSALab1/Lab1Q2.cs
@@ -9,22 +9,30 @@ namespace WSALab1
   public static class Lab1Q2
     {
         public static int VocSize;
+        public static List<WordCollection> WordsCollection { get; set; }
+
         public static List<WordCollection> FindCollection(string[] text)
         {
             var uniqueTokens = text.GroupBy(z => z).Where(z => z.Count() == 1).OrderByDescending(z => z.Count()).Select(z=>z).ToList();
-            List<WordCollection> lst = (from u in uniqueTokens
+            WordsCollection = (from u in uniqueTokens
                                        select new WordCollection
                                        {
                                            Key = u.Key,
                                            Count = u.Count()
                                        }).ToList();
-            VocSize = lst.Count();
-            return lst;
+            VocSize = WordsCollection.Count();
+           
+            return WordsCollection;
         }
 
         public static int VocabularySize()
         {
             return VocSize;
+        }
+
+        public static List<WordCollection> TopWords()
+        {
+            return WordsCollection.Take(10).ToList();
         }
 
     }
